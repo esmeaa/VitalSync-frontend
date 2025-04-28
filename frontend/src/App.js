cc970ca7bc726061c394c8ae57d10bc62cfe95cf
jamie2tu <152534939+jamie2tu@users.noreply.github.com>
2025-04-28 14:56:07 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,13 @@
 import './App.css';
 import SetUp from './pages/setUp';
 import Launch from './pages/launch';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import AuthPage from './pages/AuthPage';
-=======
-import profilepage from './pages/profilepage';
->>>>>>> Stashed changes
-=======
-import profilepage from './pages/profilepage';
->>>>>>> Stashed changes
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    <AuthPage/>
-=======
-    <profilepage/>
-
->>>>>>> Stashed changes
-=======
-    <profilepage/>
-
->>>>>>> Stashed changes
-
-
- 
+      <ProfilePage />
     </div>
   );
 }
