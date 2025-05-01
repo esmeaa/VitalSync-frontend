96acdee6ec7bf6b29d811e23cab491e5993d63e2
esmeaa <[earthuraidoo@gmail.com protected]>
2025-05-01 14:16:18 +0100

    set up navigation(temporary)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { Routes, Route, Link} from 'react-router-dom';
 import SetUp from './pages/setUp';
 import Launch from './pages/launch';
 import AuthPage from './pages/AuthPage';
@@ -8,7 +8,24 @@ import ProfilePage from './pages/profilepage';
 function App() {
   return (
     <div className="App">
-      <AuthPage/>
+       {/* Navigation */}
+       <nav>
+        <ul style={{ display: 'flex', listStyle: 'none', gap: '1rem', padding: 0 }}>
+          <li><Link to="/setUp" className="nav-link">Go to SetUp</Link></li>
+          <li><Link to="/launch" className="nav-link">Go to Launch</Link></li>
+          <li><Link to="/AuthPage" className="nav-link">Go to AuthPage</Link></li>
+          <li><Link to="/ProfilePage" className="nav-link">Go to ProfilePage</Link></li>
+
+        </ul>
+
+      </nav>
+
+      <Routes>
+	     <Route path="/setUp" element={<SetUp />} />
+	     <Route path="/launch" element={<Launch /> }/>
+       <Route path="/AuthPage" element={<AuthPage /> }/>
+       <Route path="/ProfilePage" element={<ProfilePage /> }/>
+	    </Routes>
       
     </div>
   );
