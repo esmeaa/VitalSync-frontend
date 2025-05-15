503b224ad4222a890ab95f565d7b21e8f4a66a13
jamie2tu <152534939+jamie2tu@users.noreply.github.com>
2025-05-15 18:44:19 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,7 +48,7 @@ import Navbar from './Navbar'; // Import the Navbar
 import SetUp from './pages/setUp';
 import Launch from './pages/launch';
 import AuthPage from './pages/AuthPage';
-import ProfilePage from './pages/profilepage';
+import ProfilePage from './pages/ProfilePage';
 import EditProfilePage from './pages/EditProfilePage';
 import MealPlans from './pages/MealPlans';
 import { Link } from 'react-router-dom';
